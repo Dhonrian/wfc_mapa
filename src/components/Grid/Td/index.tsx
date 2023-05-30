d61cb5ed2226a64f88d1b0165fa10b43077fbead
Leonardo Tamanhao <leonardo.tamanhao>
2023-05-29 21:07:33 -0300

    ✨ updated code to choose from set options

--- src/components/Grid/Td/index.tsx
+++ src/components/Grid/Td/index.tsx
@@ -5,11 +5,24 @@ import MenuItem from '@mui/material/MenuItem';
 
 export function Td(props: {col: number}){
 
+    const options = ['Limpar', 'Floresta', 'Grama', 'Praia', 'Oceano'];
+
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
     const open = Boolean(anchorEl);
-    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+   
+    const handleClickListItem = (event: React.MouseEvent<HTMLElement>) => {
       setAnchorEl(event.currentTarget);
     };
+
+    const handleMenuItemClick = (
+      event: React.MouseEvent<HTMLElement>,
+      index: number,
+    ) => {
+      setSelectedIndex(index);
+      setAnchorEl(null);
+    };
+
     const handleClose = () => {
       setAnchorEl(null);
     };
@@ -21,18 +34,23 @@ export function Td(props: {col: number}){
                 aria-controls={open ? `id_elem_${props.col}` : undefined}
                 aria-haspopup="true"
                 aria-expanded={open ? 'true' : undefined}
-                onClick={handleClick}></td>
+                onClick={handleClickListItem}>
+                  {selectedIndex === 0 ? '' : options[selectedIndex]}
+                 </td>
         
         <Menu id="basic-menu"
                 anchorEl={anchorEl}
                 open={open}
                 onClose={handleClose}
                 MenuListProps={{
-                'aria-labelledby': `id_elem_${props.col}`,
+                'aria-labelledby': `id_elem_${props.col}`
                 }}>
-                <MenuItem onClick={handleClose}>Menu</MenuItem>
-                <MenuItem onClick={handleClose}>Muito Foda</MenuItem>
-                <MenuItem onClick={handleClose}>Irado</MenuItem>
+                {options.map((opt, i) => (
+                  <MenuItem key={opt}
+                   selected = {i === selectedIndex}
+                   onClick={(e) => handleMenuItemClick(e, i)}
+                   >{opt}</MenuItem>
+                ))}
              </Menu>
         </Fragment>
     )
